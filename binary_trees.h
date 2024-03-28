a497f9ac439c6048a93932f1e711ef34671d9a8f
imanewkm <imanewkm02@gmail.com>
2024-03-28 16:32:17 -0700

    16-binary_tree_is_perfect.c

--- binary_trees.h
+++ binary_trees.h
@@ -80,4 +80,11 @@ char *convert(unsigned long int num, int base, int lowercase);
 bst_t *swap(bst_t *a, bst_t *b);
 size_t _binary_tree_height(const binary_tree_t *tree);
 
+unsigned char is_leaf(const binary_tree_t *node);
+size_t depth(const binary_tree_t *tree);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
 #endif
