683abb968cdd1829e168c7e3f5ed379a01c7f913
AnasOudadsse <135606982+AnasOudadsse@users.noreply.github.com>
2024-03-28 22:17:33 +0000

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -68,6 +68,7 @@ avl_t *balance_left(avl_t *node);
 avl_t *balance_right(avl_t *node);
 char *convert(unsigned long int num, int base, int lowercase);
 bst_t *swap(bst_t *a, bst_t *b);
+size_t _binary_tree_height(const binary_tree_t *tree);
 
 
 
