16ddc3ce979c657080be8b65f324a633d394bace
imanewkm <imanewkm02@gmail.com>
2024-03-29 05:16:25 -0700

    130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_size - measures the size of a binary tree
  *
@@ -8,9 +7,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
@@ -24,16 +22,13 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
-
 	if (i >= cnodes)
 		return (0);
-
 	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
 		tree_is_complete(tree->right, (2 * i) + 2, cnodes));
 }
-
 
 /**
  * binary_tree_is_complete - calls to tree_is_complete function
@@ -45,11 +40,9 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	size_t cnodes;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
 	cnodes = binary_tree_size(tree);
-
 	return (tree_is_complete(tree, 0, cnodes));
 }
 
@@ -61,12 +54,10 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  */
 int check_parent(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
-
 	if (tree->n > tree->parent->n)
 		return (0);
-
 	return (check_parent(tree->left) && check_parent(tree->right));
 }
 
@@ -80,6 +71,5 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 {
 	if (!binary_tree_is_complete(tree))
 		return (0);
-
 	return (check_parent(tree->left) && check_parent(tree->right));
 }
